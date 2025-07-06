539394bd5f14b3840e6e967974f75b4cb474e8b1
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-07-06 13:47:55 +0300

    first

---
+++ script.js
@@ -0,0 +1,3 @@
+
+document.querySelector('.section__map').href = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9866344afd1225e183015ea8586ee72066b8d137c7d612bfdc9f38222e8a3dc2&amp;width=789&amp;height=514&amp;lang=ru_RU&amp;scroll=true'
+
