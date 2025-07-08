f68c7938c9c8486fec6786bd4519bde2bb89fd39
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-07-08 15:41:35 +0300

    finish

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 
-document.querySelector('.section__map').href = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9866344afd1225e183015ea8586ee72066b8d137c7d612bfdc9f38222e8a3dc2&amp;width=789&amp;height=514&amp;lang=ru_RU&amp;scroll=true'
+const navButtons = document.querySelectorAll('.nav__button')
+    
+for(let i = 0; i < navButtons.length; i++){
+  navButtons[i].onclick = function(){
+    [...navButtons].forEach((el) => el.classList.remove('active'));
+    this.classList.add('active')
+  }
+}
 
+const buttons = document.querySelector('.nav');
+const panels = document.querySelectorAll('.panel');
+buttons.addEventListener('click', handleClick);
+function handleClick(e) {
+  if (e.target.matches('button')) {
+    panels.forEach(panel => panel.classList.remove('show'));
+    const { id } = e.target.dataset;
+    const selector = `.panel[id="${id}"]`;
+    document.querySelector(selector).classList.add('show');
+  }
+}
